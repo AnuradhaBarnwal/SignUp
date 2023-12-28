1d3c21ddab5423a8d40eadba6b1bee2354fb3391
AnuradhaBarnwal <anu.barnwal24@gmail.com>
2023-12-28 17:55:48 +0530

    signup page ui done

--- src/Components/LoginSignUp/LoginSignUp.jsx
+++ src/Components/LoginSignUp/LoginSignUp.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import './LoginSignUp.css'
-
+import React from "react";
+import "./LoginSignUp.css";
+import user_icon from "../Assets/person.png";
+import email_icon from "../Assets/email.png";
+import password_icon from "../Assets/password.png";
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
-  )
-}
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign Up</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <img src={user_icon} alt="" />
+          <input type="text" />
+        </div>
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" />
+        </div>
+      </div>
+      <div className='forget-password'>Forget password?</div>
+      <div className='submit-container'>
+      <div className='submit'>Sign Up</div>
+      <div className='submit'>Login</div>
+      </div>
+    </div>
+  );
+};
 
-export default LoginSignUp
+export default LoginSignUp;
